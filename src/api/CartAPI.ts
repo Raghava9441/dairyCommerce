a989a4170bd0ece864958833a4d45710429d68a4
Raghavendra Reddy <raghavendra.reddy@htown-tech.com>
2024-06-05 16:06:15 +0530

    cart api endpoints ready

--- src/api/CartAPI.ts
+++ src/api/CartAPI.ts
@@ -1,33 +1,58 @@
+import { api } from "./configs/axiosConfigs"
 import { defineCancelApiObject, CancelApiObject, ApiObject } from "./configs/axiosUtils" // Ensure ApiObject is exported
 
-
 export const CartAPI = {
-    getUserCart: async function () {
+    getUserCart: async function (cancel = false) {
         try {
-
+            const response = await api.request({
+                url: `/ecommerce/cart`,
+                method: "GET",
+                signal: cancel ? cancelApiObject[this.getUserCart.name].handleRequestCancellation().signal : undefined,
+            })
+            return response.data
         } catch (error) {
-
+            console.log("error:", error)
+            throw error
         }
     },
-    addItemOrUpdateQty: async function () {
+    addItemOrUpdateQty: async function (id: string, data: any, cancel = false) {
         try {
-
+            const response = await api.request({
+                url: `/ecommerce/cart/item/${id}`,
+                method: "POST",
+                data: data,
+                signal: cancel ? cancelApiObject[this.addItemOrUpdateQty.name].handleRequestCancellation().signal : undefined,
+            })
+            return response.data
         } catch (error) {
-
+            console.log("error:", error)
+            throw error
         }
     },
-    RemoveItemFromCart: async function () {
+    removeItemFromCart: async function (id: string, cancel = false) {
         try {
-
+            const response = await api.request({
+                url: `/ecommerce/cart/item/${id}`,
+                method: "DELETE",
+                signal: cancel ? cancelApiObject[this.removeItemFromCart.name].handleRequestCancellation().signal : undefined,
+            })
+            return response.data
         } catch (error) {
-
+            console.log("error:", error)
+            throw error
         }
     },
-    clearCart: async function () {
+    clearCart: async function (cancel = false) {
         try {
-
+            const response = await api.request({
+                url: `/ecommerce/cart/clear`,
+                method: "DELETE",
+                signal: cancel ? cancelApiObject[this.clearCart.name].handleRequestCancellation().signal : undefined,
+            })
+            return response.data
         } catch (error) {
-
+            console.log("error:", error)
+            throw error
         }
     },
 }
